6f906a3dd395161399476cd291f3183e6fa3ae4a
flarn2006 <flarn2006@gmail.com>
2019-10-23 23:53:42 -0400

    feat: add dialog fundament replacer

--- BetterVertexMover/Main.cs
+++ BetterVertexMover/Main.cs
@@ -57,13 +57,13 @@ namespace AnylandMods.BetterVertexMover
         {
             if (!(Main.undoStack is null))
                 Main.undoStack.Clear();
-            __instance.AddButton("undo", null, null, "ButtonVerySmall", 390, 420, "undo");
-            __instance.AddButton("falloffSmooth", null, "Smooth", "ButtonSmallCentered", 350, -225, textSizeFactor: 0.75f, textColor: TextColor.Blue);
-            __instance.AddButton("falloffDome", null, "Dome", "ButtonSmallCentered", 350, -150, textColor: TextColor.Blue);
-            __instance.AddButton("falloffLinear", null, "Linear", "ButtonSmallCentered", 350, -75, textSizeFactor: 0.75f, textColor: TextColor.Blue);
-            __instance.AddButton("falloffSharp", null, "Sharp", "ButtonSmallCentered", 350, 0, textColor: TextColor.Blue);
-            __instance.AddButton("falloffConstant", null, "Constant", "ButtonSmallCentered", 350, 75, textSizeFactor: 0.6f, textColor: TextColor.Blue);
-            __instance.AddButton("invert", null, "Invert", "ButtonSmallCentered", -400, 300, textSizeFactor: 0.75f, textColor: TextColor.Blue);
+            __instance.AddButton("undo", null, null, "ButtonVerySmall", 0, 420, "undo");
+            __instance.AddButton("falloffSmooth", null, "Smooth", "ButtonSmallCentered", 250, -225, textSizeFactor: 0.75f, textColor: TextColor.Blue);
+            __instance.AddButton("falloffDome", null, "Dome", "ButtonSmallCentered", 350, -225, textColor: TextColor.Blue);
+            __instance.AddButton("falloffLinear", null, "Linear", "ButtonSmallCentered", 450, -225, textSizeFactor: 0.75f, textColor: TextColor.Blue);
+            __instance.AddButton("falloffSharp", null, "Sharp", "ButtonSmallCentered", 250, -150, textColor: TextColor.Blue);
+            __instance.AddButton("falloffConstant", null, "Constant", "ButtonSmallCentered", 350, -150, textSizeFactor: 0.6f, textColor: TextColor.Blue);
+            __instance.AddButton("invert", null, "Invert", "ButtonSmallCentered", 450, -150, textSizeFactor: 0.75f, textColor: TextColor.Blue);
             __instance.AddSlider("Area Radius: ", "", 0, 30, 0, 4, false, Main.falloff.Radius, new Action<float>(RadiusSliderChange));
         }
     }
---
+++ PersonalizedUI/Main.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityModManagerNet;
+using Harmony;
+using UnityEngine;
+
+namespace AnylandMods.PersonalizedUI
+{
+    public static class Main
+    {
+        public static bool enabled;
+        public static UnityModManager.ModEntry mod;
+        internal static Config config;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll();
+            mod = modEntry;
+
+            config = new Config();
+            config.Load();
+
+            var btnReload = new MenuButton("personalizedUiReloadConfig", "Reload UI Config");
+            btnReload.Action += BtnReload_Action;
+            Foundation.ModMenu.Add(btnReload);
+
+            return true;
+        }
+
+        private static void BtnReload_Action(string id, Dialog dialog)
+        {
+            config.Load();
+        }
+    }
+
+    [HarmonyPatch(typeof(Dialog), "AddFundament")]
+    public static class FundamentalReplacement {
+        public static bool Prefix()
+        {
+            return !Main.config.HideFundament;
+        }
+
+        public static void Postfix(Dialog __instance)
+        {
+            if (Main.config.FundamentTID.Length > 0) {
+                Managers.thingManager.InstantiateThingOnDialogViaCache(
+                    ThingRequestContext.LocalTest,
+                    thingId: Main.config.FundamentTID,
+                    fundament: __instance.transform,
+                    position: Vector3.zero,
+                    scale: 1.0f,
+                    useDefaultRotation: true
+                );
+            }
+        }
+    }
+}
