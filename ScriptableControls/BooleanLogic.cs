2e5a8af95ad78faf66a5e1c7363661bab767c49b
flarn2006 <flarn2006@gmail.com>
2019-10-26 20:24:27 -0400

    feat: add more Boolean logic stuff

--- ScriptableControls/BooleanLogic.cs
+++ ScriptableControls/BooleanLogic.cs
@@ -4,49 +4,146 @@ using System.Linq;
 using System.Text;
 
 namespace AnylandMods.ScriptableControls {
-    struct MaskTest {
-        public enum Mode {
-            And = 0,
-            Or = 1
-        };
+    enum BooleanOp {
+        And = 0,
+        Or = 1
+    };
 
-        public UInt64 flags;
-        public UInt64 mask;
-        public Mode mode;
+    interface IFlagTest {
+        bool Evaluate(UInt64 currentFlags);
+        IFlagTest Complement { get; }
+    }
 
-        public MaskTest(UInt64 flags, UInt64 mask, Mode mode = Mode.And)
+    class MaskTest : IFlagTest {
+        public UInt64 Flags { get; private set; }
+        public UInt64 Mask { get; private set; }
+        public BooleanOp Mode { get; private set; }
+
+        public static MaskTest Tautology { get; private set; }
+        public static MaskTest Falsehood { get; private set; }
+        
+        static MaskTest()
         {
-            this.flags = flags;
-            this.mask = mask;
-            this.mode = mode;
+            Tautology = new MaskTest(0, 0, BooleanOp.And);
+            Falsehood = new MaskTest(0, 0, BooleanOp.Or);
         }
 
-        public MaskTest(UInt64 flags, Mode mode = Mode.And)
+        public MaskTest(UInt64 flags, UInt64 mask, BooleanOp mode = BooleanOp.And)
+        {
+            this.Flags = flags;
+            this.Mask = mask;
+            this.Mode = mode;
+        }
+
+        public MaskTest(UInt64 flags, BooleanOp mode = BooleanOp.And)
             : this(flags, flags, mode)
         { }
 
         public bool Evaluate(UInt64 currentFlags)
         {
-            if (mode == Mode.And)
-                return (currentFlags & mask) == (flags & mask);
+            if (Mode == BooleanOp.And)
+                return (currentFlags & Mask) == (Flags & Mask);
             else // Mode.Or
-                return (~(currentFlags ^ flags) & mask) == 0;
+                return Mask != 0 && (~(currentFlags ^ Flags) & Mask) == 0;
         }
 
-        private static Mode OppositeMode(Mode mode)
+        internal static BooleanOp OppositeMode(BooleanOp mode)
         {
-            return mode == Mode.And ? Mode.Or : Mode.And;
+            return mode == BooleanOp.And ? BooleanOp.Or : BooleanOp.And;
         }
 
         public MaskTest Complement {
             get {
-                return new MaskTest(~flags, mask, OppositeMode(mode));
+                return new MaskTest(~Flags, Mask, OppositeMode(Mode));
+            }
+        }
+
+        IFlagTest IFlagTest.Complement {
+            get {
+                return Complement;
             }
         }
 
         public static MaskTest operator!(MaskTest target)
         {
             return target.Complement;
+        }
+
+        public static CompoundTest operator|(MaskTest a, IFlagTest b)
+        {
+            var tests = new IFlagTest[] { a, b };
+            return new CompoundTest(tests, BooleanOp.Or);
+        }
+
+        public static CompoundTest operator&(MaskTest a, IFlagTest b)
+        {
+            var tests = new IFlagTest[] { a, b };
+            return new CompoundTest(tests, BooleanOp.And);
+        }
+    }
+
+    class CompoundTest : IFlagTest {
+        private IFlagTest[] tests;
+        private BooleanOp mode;
+
+        private CompoundTest() { }
+
+        public CompoundTest(IEnumerable<IFlagTest> tests, BooleanOp mode = BooleanOp.And)
+        {
+            this.tests = tests.ToArray();
+            this.mode = mode;
+        }
+
+        public bool Evaluate(UInt64 currentFlags)
+        {
+            if (mode == BooleanOp.And) {
+                foreach (IFlagTest test in tests) {
+                    if (!test.Evaluate(currentFlags)) return false;
+                }
+                return true;
+            } else {
+                foreach (IFlagTest test in tests) {
+                    if (test.Evaluate(currentFlags)) return true;
+                }
+                return false;
+            }
+        }
+
+        private static IFlagTest FlagTestToComplement(IFlagTest test)
+        {
+            return test.Complement;
+        }
+
+        public CompoundTest Complement {
+            get {
+                var ct = new CompoundTest();
+                ct.tests = tests.Select(FlagTestToComplement).ToArray();
+                ct.mode = MaskTest.OppositeMode(mode);
+                return ct;
+            }
+        }
+
+        IFlagTest IFlagTest.Complement {
+            get {
+                return Complement;
+            }
+        }
+
+        public static CompoundTest operator!(CompoundTest target)
+        {
+            return target.Complement;
+        }
+
+        public static CompoundTest operator|(CompoundTest a, IFlagTest b)
+        {
+            var tests = new IFlagTest[] { a, b };
+            return new CompoundTest(tests, BooleanOp.Or);
+        }
+
+        public static CompoundTest operator&(CompoundTest a, IFlagTest b)
+        {
+            var tests = new IFlagTest[] { a, b };
+            return new CompoundTest(tests, BooleanOp.And);
         }
     }
 }
---
+++ ScriptableControls/ControlState.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AnylandMods.ScriptableControls {
+    static class ControlState {
+        public static class Flags {
+            public const UInt64 ContextLaser = 1;
+            public const UInt64 Delete = 2;
+            public const UInt64 FingersClosed = 4;
+            public const UInt64 LegControl = 8;
+            public const UInt64 TeleportLaser = 16;
+            public const UInt64 Trigger = 32;
+            public const int BitsToShiftForLeft = 6;
+
+            public static UInt64 BitValueForLetter(char letter)
+            {
+                switch (letter) {
+                    case 'c': return ContextLaser;
+                    case 'd': return Delete;
+                    case 'f': return FingersClosed;
+                    case 'l': return LegControl;
+                    case 'p': return TeleportLaser;
+                    case 't': return Trigger;
+                    default: return 0;
+                }
+            }
+        }
+
+        private static Regex tellRegex;
+        
+        static ControlState()
+        {
+            tellRegex = new Regex("^xc([blr]?)([01]) ?([cdflpt]*)-?([cdflpt]*)$");
+        }
+
+        public static IFlagTest ParseTellString(string tell)
+        {
+            Match match = tellRegex.Match(tell);
+            if (!match.Success) return MaskTest.Falsehood;
+
+            string p_side = match.Captures[0].Value;
+            char p_state = match.Captures[1].Value[0];
+            string p_true = match.Captures[2].Value;
+            string p_false = match.Captures[3].Value;
+
+            UInt64 flags = 0;
+            UInt64 mask = 0;
+
+            foreach (char c in p_true) {
+                flags |= Flags.BitValueForLetter(c);
+            }
+            mask = flags;
+            foreach (char c in p_false) {
+                UInt64 bit = Flags.BitValueForLetter(c);
+                if ((flags & bit) != 0) {
+                    // None of them can ever be both true and false.
+                    return MaskTest.Falsehood;
+                }
+            }
+
+            var left = new MaskTest(flags << Flags.BitsToShiftForLeft, mask << Flags.BitsToShiftForLeft);
+            var right = new MaskTest(flags, mask);
+
+            if (p_state == '1') {
+                switch (p_side) {
+                    case "b": return left & right;
+                    case "l": return left;
+                    case "r": return right;
+                    case "": return left | right;
+                    default: return MaskTest.Falsehood;
+                }
+            } else {
+                switch (p_side) {
+                    case "b": return !(left & right);
+                    case "l": return !left;
+                    case "r": return !right;
+                    case "": return !(left | right);
+                    default: return MaskTest.Falsehood;
+                }
+            }
+        }
+    }
+}
