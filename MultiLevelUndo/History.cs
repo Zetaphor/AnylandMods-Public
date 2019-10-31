d49261f14a1e005303ef82afb980457ecab709a8
flarn2006 <flarn2006@gmail.com>
2019-10-30 20:12:43 -0400

    feat: add MultiLevelUndo (doesn't work yet)

---
+++ MultiLevelUndo/History.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AnylandMods.MultiLevelUndo {
+
+    struct ThingPartStateHistoryEntry {
+        public Vector3 position;
+        public Vector3 rotation;
+        public Vector3 scale;
+        public Color color;
+
+        public ThingPartStateHistoryEntry(Vector3 position, Vector3 rotation, Vector3 scale, Color color)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+            this.color = color;
+        }
+
+        public ThingPartStateHistoryEntry(ThingPartState state)
+        {
+            position = state.position;
+            rotation = state.rotation;
+            scale = state.scale;
+            color = state.color;
+        }
+    }
+
+    public class UndoHistory<T> {
+        private Stack<T> past, future;
+
+        public UndoHistory()
+        {
+            past = new Stack<T>();
+            future = new Stack<T>();
+        }
+
+        public int UndoCount {
+            get {
+                return past.Count;
+            }
+        }
+
+        public int RedoCount {
+            get {
+                return future.Count;
+            }
+        }
+
+        public T Undo()
+        {
+            T state = past.Pop();
+            future.Push(state);
+            return state;
+        }
+
+        public T Redo()
+        {
+            T state = future.Pop();
+            past.Push(state);
+            return state;
+        }
+
+        public void PushState(T state)
+        {
+            past.Push(state);
+            future.Clear();
+        }
+    }
+
+    public class HistoryBook<TSubject, THistEntry> {
+        public delegate string IdentityFunc(TSubject obj);
+
+        private Dictionary<string, UndoHistory<THistEntry>> dict;
+
+        public IdentityFunc Identity { get; private set; }
+
+        public HistoryBook(IdentityFunc idfunc)
+        {
+            Identity = idfunc;
+            dict = new Dictionary<string, UndoHistory<THistEntry>>();
+        }
+
+        public UndoHistory<THistEntry> GetHistory(TSubject subject)
+        {
+            string id = Identity(subject);
+            try {
+                return dict[id];
+            } catch (KeyNotFoundException) {
+                var hist = new UndoHistory<THistEntry>();
+                dict[id] = hist;
+                return hist;
+            }
+        }
+
+        public int UndoCount(TSubject subject) => GetHistory(subject).UndoCount;
+        public int RedoCount(TSubject subject) => GetHistory(subject).RedoCount;
+        public THistEntry Undo(TSubject subject) => GetHistory(subject).Undo();
+        public THistEntry Redo(TSubject subject) => GetHistory(subject).Redo();
+        public void PushState(TSubject subject, THistEntry state) => GetHistory(subject).PushState(state);
+    }
+
+}
---
+++ MultiLevelUndo/Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityModManagerNet;
+using UnityEngine;
+using Harmony;
+
+namespace AnylandMods.MultiLevelUndo
+{
+    public static class Main
+    {
+        internal static HistoryBook<Tuple<ThingPart, int>, ThingPartStateHistoryEntry> thingPartStateHistory;
+
+        public static bool enabled;
+        public static UnityModManager.ModEntry mod;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            thingPartStateHistory = new HistoryBook<Tuple<ThingPart, int>, ThingPartStateHistoryEntry>(IdentifyThingPartState);
+
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll();
+
+            mod = modEntry;
+            return true;
+        }
+
+        private static string IdentifyThingPartState(Tuple<ThingPart, int> thingPartState)
+        {
+            return String.Format("{0}:{1}", thingPartState.Item1.guid, thingPartState.Item2);
+        }
+
+        internal static Tuple<ThingPart, int> GetThingPartStateTuple(ThingPart thingPart) => new Tuple<ThingPart, int>(thingPart, thingPart.currentState);
+    }
+
+    [HarmonyPatch(typeof(ThingPart), "MemorizeForUndo")]
+    public static class RewriteThingPartMemorizeForUndo {
+        public static bool Prefix(ThingPart __instance)
+        {
+            if (__instance.GetIsOfThingBeingEdited()) {
+                if (__instance.currentState > __instance.states.Count - 1) {
+                    __instance.currentState = 0;
+                }
+                var tuple = Main.GetThingPartStateTuple(__instance);
+                Main.thingPartStateHistory.PushState(tuple, new ThingPartStateHistoryEntry(__instance.states[__instance.currentState]));
+            }
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ThingPart), nameof(ThingPart.Undo))]
+    public static class RewriteThingPartUndo {
+        public static bool Prefix(ThingPart __instance)
+        {
+            var tuple = Main.GetThingPartStateTuple(__instance);
+            if (Main.thingPartStateHistory.UndoCount(tuple) > 0) {
+                ThingPartStateHistoryEntry saved = Main.thingPartStateHistory.Undo(tuple);
+                __instance.transform.localPosition = saved.position;
+                __instance.transform.localEulerAngles = saved.rotation;
+                __instance.transform.localScale = saved.scale;
+                __instance.material.color = saved.color;
+                __instance.SetStatePropertiesByTransform(false);
+                if (__instance.materialType == MaterialTypes.Transparent)
+                    __instance.UpdateMaterial();
+            } else {
+                Managers.soundManager.Play("no", __instance.transform, 0.5f, false, false);
+            }
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ThingPart), nameof(ThingPart.HasUndoForThisState))]
+    public static class RewriteThingPartHasUndoForThisState {
+        public static bool Prefix(ThingPart __instance, ref bool __result)
+        {
+            __result = Main.thingPartStateHistory.UndoCount(Main.GetThingPartStateTuple(__instance)) > 0;
+            return false;
+        }
+    }
+}
